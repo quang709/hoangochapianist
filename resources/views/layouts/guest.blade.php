6c74ccec685d975d09b3abf79841b66aff6f7a99
quang709 <quangquangquang321@gmail.com>
2022-09-16 11:35:59 +0700

    update

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,7 +11,6 @@
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Scripts -->
-     
         <link rel="stylesheet" href="{{ mix('resources/css/app.css', 'app') }}">
          <link rel="stylesheet" href="{{ mix('resources/css/app.css', 'app') }}">
     </head>
