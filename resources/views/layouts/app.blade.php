ecfa3209a969a62ea6e0b073984532da6a65048e
quang709 <quangquangquang321@gmail.com>
2022-09-16 11:50:29 +0700

    upload

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,18 +9,18 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
-        <!-- Styles -->
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-     
+
      
         <!-- Fonts -->
        <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
 
+ 
 
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
+        <link rel="stylesheet" href="{{ asset('app/css/app.css') }}">
+
+<script src="{{ asset('app/js/app.js') }}" type="text/javascript"></script>
     </head>
     <body>
 
