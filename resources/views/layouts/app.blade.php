39ed3bebca7028302b511878ff451841d205c2f6
quang709 <quangquangquang321@gmail.com>
2022-09-16 11:52:01 +0700

    upload

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,11 +16,12 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
 
- 
-
-        <link rel="stylesheet" href="{{ asset('app/css/app.css') }}">
-
-<script src="{{ asset('app/js/app.js') }}" type="text/javascript"></script>
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+     
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+        
     </head>
     <body>
 
