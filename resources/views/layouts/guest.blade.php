43101bea5313d6fc9bc04f24558687d22f396882
quang709 <quangquangquang321@gmail.com>
2022-09-16 11:41:37 +0700

    test

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,6 +12,8 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        
     </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">
