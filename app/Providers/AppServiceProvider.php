a846a3ea02418b5620116ecfe1507525694852fb
quang709 <quangquangquang321@gmail.com>
2022-09-16 11:50:41 +0700

    upload

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->app->bind('path.public', function() {
-            return realpath(base_path().'/../public_html');
-        });
+        //
     }
 }
